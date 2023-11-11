8ef0899f6fbe57f7adeb8d1a0112a4cab60751fd
Aga Palaganas <149645611+MioneHiel@users.noreply.github.com>
2023-11-10 20:33:11 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Accounting
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+    
