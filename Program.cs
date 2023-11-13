5be9f60217ed40d1d5481d7e44ee350a0a03a429
Aga Palaganas <149645611+MioneHiel@users.noreply.github.com>
2023-11-12 19:02:50 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace MathComparisonOperator
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate?");
+            string hourlyRate1 = Console.ReadLine();
+            Console.WriteLine("Hours worked per week");
+            string hours1 = Console.ReadLine();
+            int salary1 = Convert.ToInt32(hourlyRate1) * Convert.ToInt32(hours1) * 52;
+            Console.WriteLine("Person2");
+            Console.WriteLine("Hourly Rate?");
+            string hourlyRate2 = Console.ReadLine();
+            Console.WriteLine("Hours worked per week");
+            string hours2 = Console.ReadLine();
+            int salary2 = Convert.ToInt32(hourlyRate2) * Convert.ToInt32(hours2) * 52;
+            Console.WriteLine("Annual Salary of Person 1:");
+            Console.WriteLine(salary1);
+            Console.WriteLine("Annual Salary of Person 2:");
+            Console.WriteLine(salary2);
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool isMore = salary1 > salary2;
+            Console.WriteLine(isMore);
+            Console.ReadLine();
+
+        }
+    }
+}
