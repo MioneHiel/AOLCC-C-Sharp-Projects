0e75551a6c125ff7fc604fbf8c3c8df7b9d0f361
Aga Palaganas <149645611+MioneHiel@users.noreply.github.com>
2024-03-08 21:19:34 -0700

    Add files via upload

---
+++ LambdaExpression/LambdaExpression/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace LambdaExpression
+{
+    //Create an Employee class with id, firstname and lastname properties
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
