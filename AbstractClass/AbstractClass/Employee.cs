4b327cceddc1f56aaeb00103d1eb1638950680b9
Aga Palaganas <149645611+MioneHiel@users.noreply.github.com>
2024-03-08 21:16:11 -0700

    Add files via upload

---
+++ AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    //Create another class, "Employee" and have it inherit from the Person class
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+        //Implement the SayName() method inside of the Employee class
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main method, instantiate an Employee object
+            Employee sam = new Employee() { firstName = "Sample", lastName = "Student" };
+            //Call the SayName method on the object
+            sam.SayName();
+            Console.ReadLine();
+        }
+    }
+}
